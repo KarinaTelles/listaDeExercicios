b0590b847fb960ce95bdc87988dfe03556fae842
Karina Cardoso Telles <karina@divibank.co>
2022-11-04 20:05:49 -0300

    backup

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import exercicios.dois.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+       Menu menu = new Menu();
+        System.out.println(menu.leEntradaMenuPrincipal());
+    }
+}
